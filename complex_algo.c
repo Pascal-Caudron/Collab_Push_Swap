94a9e32e1e42f36325edc8936cc016003ce9db96
Bjmrzd <bjmrzd2611@gmail.com>
2026-01-08 15:43:57 +0100

    Add files via upload

--- complex_algo.c
+++ complex_algo.c
@@ -2,10 +2,10 @@
 
 int	getMax(t_list **list)
 {
-	// int		max_index;
 	int		max;
 	t_list	*tmp;
 
+	// int		max_index;
 	tmp = (*list);
 	max = tmp->data;
 	// max_index = 0;
@@ -19,8 +19,8 @@ int	getMax(t_list **list)
 	// while (tmp)
 	// {
 	// 	if (tmp->data == max)
-	// 		return (++max_index);
-	// 	max_index++;
+	// 		// return (++max_index);
+	// 	// max_index++;
 	// 	tmp = tmp->next;
 	// }
 	return (max);
@@ -41,44 +41,51 @@ int	max_bits(t_list **list)
 	return (bits);
 }
 
+// int	getBits(int digit, int pos)
+// {
+// 	int	power;
+// 	int	count;
+
+// 	count = 0;
+// 	power = 1;
+// 	while (count < pos)
+// 	{
+// 		power *= 2;
+// 		count++;
+// 	}
+// 	return ((digit / power) % 2);
+// }
+
 int	getBits(int digit, int pos)
 {
-	int	power;
-	int	count;
-
-	count = 0;
-	power = 1;
-	while (count < pos)
-	{
-		power *= 2;
-		count++;
-	}
-	return ((digit / power) % 2);
+	return ((digit >> pos) & 1);
 }
-
 void	radix_sort(t_list **b_list, t_list **a_list, t_count *count)
 {
-	int	m_bits;
-	int	size;
-	int	count_bits;
-	int	index;
-	int	bits;
+	int		m_bits;
+	int		size;
+	int		count_bits;
+	int		index;
+	t_list	*tmp;
+	int		bits;
 
-	
 	size = ft_lstsize((*a_list));
 	m_bits = max_bits(a_list);
 	count_bits = 0;
-	while (count_bits++ < m_bits)
+	while (count_bits < m_bits)
 	{
 		index = 0;
-		while (index++ < size)
+		while (index < size)
 		{
-			bits = getBits((*a_list)->data, count_bits);
+			tmp = (*a_list);
+			bits = getBits(tmp->data, count_bits);
 			if (bits == 0)
-				ra(a_list, count);
-			else
 				pb(a_list, b_list, count);
+			else
+				ra(a_list, count);
+			index++;
 		}
+		count_bits++;
 	}
 	while (*b_list)
 		pa(a_list, b_list, count);
